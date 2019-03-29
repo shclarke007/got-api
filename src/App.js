41745c3119ec65f7c2814959495ca0d3a0fdfa49
Shaneil Clarke <shaneilske@yahoo.com>
2019-03-29 15:01:12 +0000

    added constructor function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 // import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items:[],
+      isLoaded:false,
+    }
+  }
   
+  render() {
+    return (
+      <div className="App">
+        <header><h1>Game of Thrones API</h1></header>
+
+      </div>
+    );
+  }
 }
 
 export default App;
