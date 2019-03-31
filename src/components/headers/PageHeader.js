5ae5c3dfb1f989144a7cedc581be4954a3590c9e
Shaneil Clarke <shaneilske@yahoo.com>
2019-03-31 21:22:16 +0100

    added page header

---
+++ src/components/headers/PageHeader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PageHeader extends Component {
+  render() {
+    return (
+      <h1>Game of Thrones API</h1>
+      )
+  }
+}
+
+export default PageHeader;
